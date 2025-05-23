b7d26d626a61c66eb9e2794eb591def28e39fcc2
Kirbydude77 <dhao100@gmail.com>
2025-05-22 20:16:57 -0400

    First Test

--- Expressions.py
+++ Expressions.py
@@ -1,128 +1,145 @@
-from TypeCheckers import *
+# Expressions.py
+
+import TypeCheckers
+import types
 
 constructs = {}
 
-types = {}
+exprTypes = {}
 
 context = [{}]
 
-def addVarToContext(var):
+def add_var_to_context(var):
     context[-1][var.symbol] = var
 
-def pushLocalContext(local:dict = None):
-    local:dict = Initiate(local)
+def push_local_context(local: dict = None):
+    local = TypeCheckers.Initiate(local, dict)
     context.append(local)
 
-def popLocalContext():
+def pop_local_context():
     return context.pop()
 
-def GetVar(symbol):
+def get_var(symbol):
     for scope in reversed(context):
         if symbol in scope:
             return scope[symbol]
     return None
 
-
-
 class ExprType:
-    def __init__(self, name:str, *parents):
-        if len(parents) == 0 and name!= "any":
-            parents = [anyType]
+    def __init__(self, name: str, *parents):
+        self.name = TypeCheckers.EnsureIsType(name, str)
+        if name == "any":
+            self.parents = []
+        elif len(parents) == 0:
+            self.parents = [exprTypes["any"]]
         else:
-            self.parents = EnsureIsListOf(parents, ExprType)
-        self.name = EnsureIsType(name, str)
+            self.parents = TypeCheckers.EnsureIsListOf(list(parents), ExprType)
+        exprTypes[name] = self
 
-        types[name] = self
-
-    def subTypeOf(self, exprType):
-        if self == exprType:
+    def sub_type_of(self, expr_type):
+        if self == expr_type:
             return True
-        else: return any([parent.subTypeOf(exprType) for parent in self.parents])
-
-    # Return a Variable of this type
-    def __call__(self, symbol):
-        inContext = GetVar(symbol)
-        if inContext:
-            return inContext
         else:
-            return Var()
+            return any(parent.sub_type_of(expr_type) for parent in self.parents)
 
-anyType = ExprType("any", )
+    # Return a variable of this type
+    def __call__(self, symbol):
+        in_context = get_var(symbol)
+        if in_context:
+            return in_context
+        else:
+            return Var(self, symbol)
+
+    def __repr__(self):
+        return f"ExprType({self.name})"
+
+anyType = ExprType("any")
 
 class Construct:
-    def __init__(self, call: str, inputTypes: list, outputType: ExprType, renderFunct):
-        self.call = EnsureIsType(call, str)
-        self.inputTypes = EnsureIsListOf(inputTypes, ExprType)
-        self.numInputs = len(inputTypes)
+    def __init__(self, call: str, input_types: list, output_type: ExprType, render_funct):
+        self.call = TypeCheckers.EnsureIsType(call, str)
+        self.input_types = TypeCheckers.EnsureIsListOf(input_types, ExprType)
+        self.num_inputs = len(input_types)
 
-        self.outputType = EnsureIsType(outputType, ExprType)
+        self.output_type = TypeCheckers.EnsureIsType(output_type, ExprType)
 
-        if isinstance(renderFunct, function):        
-            self.renderFunct = EnsureIsType(renderFunct, function)
-        elif isinstance(renderFunct, str):
-            lambda renderedSubExprs: renderFunct.format(*renderedSubExprs)
+        if isinstance(render_funct, types.FunctionType):
+            self.render_funct = TypeCheckers.EnsureIsType(render_funct, types.FunctionType)
+        elif isinstance(render_funct, str):
+            self.render_funct = lambda rendered_sub_exprs: render_funct.format(*rendered_sub_exprs)
         else:
-            raise TypeError(renderFunct, "must be a str or function")
+            raise TypeError(f"{render_funct} must be a str or function")
         constructs[call] = self
 
-    def __call__(self, *subExprs):
-        EnsureIsListOf(subExprs, Expr)
-        return Expr(self, *subExprs)
-        
+    def __call__(self, *sub_exprs):
+        TypeCheckers.EnsureIsListOf(list(sub_exprs), Expr)
+        return Expr(self, *sub_exprs)
+
+    def __repr__(self):
+        return f"Construct({self.call})"
 
 class Expr:
-    def __init__(self, construct: Construct, *subExprs):
-        self.subExprs = EnsureIsListOf(subExprs, Expr)
-        self.numSubExprs = len(subExprs)
+    def __init__(self, construct: Construct, *sub_exprs):
+        self.sub_exprs = TypeCheckers.EnsureIsListOf(list(sub_exprs), Expr)
+        self.num_sub_exprs = len(sub_exprs)
 
-        if self.numSubExprs != construct.numInputs:
-            raise ValueError(subExprs, "must have length", construct.numInputs)
+        if self.num_sub_exprs != construct.num_inputs:
+            raise ValueError(f"Expected {construct.num_inputs} subexpressions but got {self.num_sub_exprs}")
 
-        for subExpr, inputType in zip(self.subExprs, construct.inputTypes):
-            if subExpr.type != inputType:
-                raise ValueError(subExpr, "is not a", inputType)
+        for sub_expr, input_type in zip(self.sub_exprs, construct.input_types):
+            if sub_expr.type != input_type:
+                raise ValueError(f"Subexpression {sub_expr} is not of type {input_type}")
 
+        self.construct = TypeCheckers.EnsureIsType(construct, Construct)
+        self.type = construct.output_type
 
-        self.construct = EnsureIsType(construct, Construct)
+        # Collect variables from all subexpressions
+        self.vars = set()
+        for sub_expr in sub_exprs:
+            self.vars.update(sub_expr.vars)
 
-        self.type = construct.outputType
-
-        self.vars = set().update(*[subExpr.vars for subExpr in subExprs])
-    
-    # First subexprs are all rendered, then plugged into the rendering function of the construct
+     # First subexprs are all rendered, then plugged into the rendering function of the construct
     def render(self):
-        renderedSubExprs = [subExpr.render() for subExpr in self.subExprs]
-        return self.construct.renderFunct(renderedSubExprs)
-    
-    # Expressions are equal when they have the same construct, and their subexpressions are equal. 
-    def __eq__(self, value):
-        if not isinstance(value, Expr) or value.construct != self.construct:
+        rendered_sub_exprs = [sub_expr.render() for sub_expr in self.sub_exprs]
+        return self.construct.render_funct(rendered_sub_exprs)
+
+     # Expressions are equal when they have the same construct, and their subexpressions are equal. 
+    def __eq__(self, other):
+        if not isinstance(other, Expr) or other.construct != self.construct:
             return False
-        return all([selfSubExpr == valueSubExpr for selfSubExpr, valueSubExpr in zip(self.subExprs, value.subExprs)])
+        return all(s == o for s, o in zip(self.sub_exprs, other.sub_exprs))
 
     def __getitem__(self, num: int):
-        return self.subExprs[num]
-    
-    def __iter__(self):
-        return self.subExprs
-    
-    def __len__(self):
-        return self.numSubExprs
+        return self.sub_exprs[num]
 
+    def __iter__(self):
+        return iter(self.sub_exprs)
+
+    def __len__(self):
+        return self.num_sub_exprs
+
+    def __repr__(self):
+        return f"Expr({self.construct.call}, {self.sub_exprs})"
 
 class Var(Expr):
-    def __init__(self, type: ExprType, symbol):
-        self.subExprs = []
-        self.numSubExprs = 0
+    def __init__(self, type_: ExprType, symbol: str):
+        self.sub_exprs = []
+        self.num_sub_exprs = 0
         self.construct = None
-        self.type: ExprType = EnsureIsType(type, ExprType)
-        self.symbol: str = EnsureIsType(symbol, str)
-
-        addVarToContext(self)
+        self.type = TypeCheckers.EnsureIsType(type_, ExprType)
+        self.symbol = TypeCheckers.EnsureIsType(symbol, str)
+        add_var_to_context(self)
+        self.vars = {self}
 
     def render(self):
         return self.symbol
-    
+
     # Variables are equal iff their id is equal, it is NOT dependent on the symbol used. 
-    def __eq__(self, value):
-        return id(self) == id(value)
+    def __eq__(self, other):
+        return id(self) == id(other)
+
+    def __hash__(self):
+        return id(self)
+
+    def __repr__(self):
+        return f"Var({self.symbol}:{self.type.name})"
--- TypeCheckers.py
+++ TypeCheckers.py
@@ -1,31 +1,31 @@
-from inspect import signature
-
 ensure = True
 
-emptyTypeDict = {dict: {}, list:[]}
-
+empty_type_dict = {dict: {}, list: []}
 
 def EnsureIsType(val, val_type: type):
     if ensure:
-        if not (isinstance(val_type, type)):
-            raise TypeError(val_type, "is not a type, is", type(val_type))
+        if not isinstance(val_type, type):
+            raise TypeError(f"{val_type} is not a type, is {type(val_type)}")
         if not isinstance(val, val_type):
-            raise TypeError(val, "is not ", val_type, "is", type(val))
+            raise TypeError(f"{val} is not {val_type}, is {type(val)}")
     return val
 
 def Initiate(val, val_type):
     EnsureIsType(val_type, type)
-    if val == None:
-        return emptyTypeDict[val_type]
+    if val is None:
+        if val_type in empty_type_dict:
+            if isinstance(empty_type_dict[val_type], dict):
+                return {}
+            elif isinstance(empty_type_dict[val_type], list):
+                return []
+        return None
     else:
-        EnsureIsType(val,val_type)
+        EnsureIsType(val, val_type)
         return val
-    
 
 def EnsureIsListOf(val: list, val_type: type):
     if ensure:
         EnsureIsType(val, list)
         for element in val:
-            EnsureIsType(val_type)
+            EnsureIsType(element, val_type)
     return val
-
